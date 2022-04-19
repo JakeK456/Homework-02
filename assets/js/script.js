da77baa9aa44b91eadae447a9e415dd81427e2f6
JK <jrileykruger@gmail.com>
2022-04-18 17:14:23 -0700

    added event listener to container, opens url based on targets data attribute

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-console.log("here i am!")
+
+const workContainerEl = $('#work-container');
+
+workContainerEl.on('click', function(event){
+    const articleParent = $(event.target).closest('article');
+    const url = articleParent.data('url');
+    openNewTab(url);
+})
+
+function openNewTab(url){
+    const win = window.open(url, '_blank');
+    if (win) {
+    win.focus();
+    } else {
+    alert('Please allow popups for this website');
+    }
+}
